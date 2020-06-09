3d1591a6794b92146926e13b7f1a36b0d39bcf53
Harish Singh <harishbisht29@gmail.com>
2020-06-10 00:22:49 +0530

    modify __main__ module

--- image_sizer.py
+++ image_sizer.py
@@ -1,6 +1,8 @@
 from PIL import Image
 from resizeimage import resizeimage
 import os
+import sys
+
 from os import listdir
 from os.path import isfile, join
 
@@ -14,7 +16,7 @@ class Image_Sizer:
         for t in template_names:
             tf= os.path.join(template_folder, t)
             self.image_dims[os.path.splitext(t)[0]] = Image.open(tf).size
-        print(self.image_dims)
+
     def __init__(self, template_loc):
         self.get_image_dims(template_loc)
 
@@ -35,39 +37,15 @@ class Image_Sizer:
 
 
 if __name__ == "__main__":
-    sizer= Image_Sizer('./templates')
-    # print(sizer.image_dims)
-    cimage= sizer.resize_image('codethemall_logo.png','signs')
-    cimage.save('codethemall_logo.png')
+	ifile_name= sys.argv[1]
+	sizer= Image_Sizer('./templates')
+	# print(sizer.image_dims)
+	# cimage= sizer.resize_image(ifile_name,'blog_cover')
+	# cimage.save('venv_image_output.jpg')
+	print(sizer.image_dims)
+	for name in sizer.image_dims:
+		cimage= sizer.resize_image(ifile_name,name)
+		cimage.save("{}_{}{}".format(os.path.splitext(ifile_name)[0],name,os.path.splitext(ifile_name)[1]))
+
+
   
-
-# template_name= 'blog_featured'
-# template_ratio = image_templates[template_name][0]/image_templates[template_name][1]
-# print("Template Ration",template_ratio)
-
-# input_image = Image.open('./test_image.jpg')
-# width = input_image.size[0]
-# print(width)
-# height = width/template_ratio
-# print(height)
-# converted_image = resizeimage.resize_cover(input_image,[width,height])
-# converted_image.save('test-image-cover.jpeg', input_image.format)
-# test_image = Image.open('./test_image.jpg')
-# cover = resizeimage.resize_thumbnail(test_image, image_templates['blog_featured'])
-# # cover.show()
-# print(blog_featured.size)
-# print(test_image.size)
-# print(cover.size)
-
-# cover.save('test-image-cover.jpeg', test_image.format)
-
-# import PIL
-# from PIL import Image
-
-# mywidth = 300
-
-# img = Image.open('someimage.jpg')
-# wpercent = (mywidth/float(img.size[0]))
-# hsize = int((float(img.size[1])*float(wpercent)))
-# img = img.resize((mywidth,hsize), PIL.Image.ANTIALIAS)
-# img.save('resized.jpg')
